08b0d21ea7e9dac6841592de067d497da020b5d2
Jerry Qi <jerryq0101@gmail.com>
2024-10-19 19:45:24 -0400

    Kinda works

--- rubiks.c
+++ rubiks.c
@@ -11,12 +11,16 @@ void update_char(int row, int col, char c);
 void setup_cube(int cube_slab[12][9]);
 void print_cube(int cube_slab[12][9]);
 void rotate_cube(int axis, char direction);
+void highlight_axis_num(int axis);
+void refresh_cube();
+void increment_axis(int up);
 
 #define EXIT_CHAR '~'
-#define INPUT_ROW 13
+#define INPUT_ROW 19
 #define INPUT_COL 1
 
 #define BLOCK "\u2588"
+#define BLOCK_HALF "\u2591"
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
 #define YELLOW "\033[0;33m"
@@ -31,6 +35,7 @@ const char* COLORS[] = {RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN};
 6 sides of a cube
 */
 int cube_slab[12][9] = {{0}};
+int cube_selected[12][9] = {{0}};
 
 /*
 9 rotation axis
@@ -45,16 +50,16 @@ int *axis_7[12] = {*(cube_slab+3)+0, *(cube_slab+3)+1, *(cube_slab + 3)+2, *(cub
 int *axis_8[12] = {*(cube_slab+4)+0, *(cube_slab+4)+1, *(cube_slab + 4)+2, *(cube_slab + 4)+3, *(cube_slab + 4)+4, *(cube_slab + 4)+5, *(cube_slab + 4)+6, *(cube_slab + 4)+7, *(cube_slab + 4)+8, *(cube_slab + 10)+5, *(cube_slab + 10)+4, *(cube_slab + 10)+3};
 int *axis_9[12] = {*(cube_slab+5)+0, *(cube_slab+5)+1, *(cube_slab + 5)+2, *(cube_slab + 5)+3, *(cube_slab + 5)+4, *(cube_slab + 5)+5, *(cube_slab + 5)+6, *(cube_slab + 5)+7, *(cube_slab + 5)+8, *(cube_slab + 11)+5, *(cube_slab + 11)+4, *(cube_slab + 11)+3};;
 
+int axis = 0;
 
-
+/* For Terminal Settings in RAW mode */
 struct termios orig_termios;
-
 
 int main(void) {
         enable_raw_mode();
-        printf("\033[2J\033[H");
 
         setup_cube(cube_slab);
+        highlight_axis_num(axis);
         printf("\033[2J\033[H");
         print_cube(cube_slab);
 
@@ -62,16 +67,13 @@ int main(void) {
         move_cursor(INPUT_ROW, INPUT_COL);
 
         int c;
-        int count = 0;
-        static int axis = 1;
-
         while ((c = getchar()) != EXIT_CHAR) {
                 switch (c) {
                         case 'w':
-                                axis = axis == 10 ? 1 : axis + 1;
+                                increment_axis(1);
                                 break;
                         case 's':
-                                axis = axis == 0 ? 9 : axis - 1;
+                                increment_axis(0);
                                 break;
                         case 'r':
                                 rotate_cube(axis, 'r');
@@ -86,11 +88,9 @@ int main(void) {
                                 rotate_cube(axis, 'd');
                                 break;
                 }
+                refresh_cube();
 
-                printf("\033[2J\033[H");
-                print_cube(cube_slab);
-
-                // Ready for input
+                // Input space
                 move_cursor(INPUT_ROW, INPUT_COL);
         }
         
@@ -99,11 +99,27 @@ int main(void) {
 }
 
 
+// Cube Functions ----------------------
+void refresh_cube() {
+        printf("\033[2J\033[H");
+        print_cube(cube_slab);
+}
+
+void increment_axis(int up) {
+        highlight_axis_num(axis);
+        if (up) {
+                axis = axis == 10 ? 0 : axis + 1;
+        } else {
+                axis = axis < 0 ? 9 : axis - 1;
+        }
+        highlight_axis_num(axis);
+}
+
 
 // Array processing --------------------
-
 void shift_array_3(int *array[], int direction);
 void print_block(int start_row, int start_col);
+void toggle_highlight(int **selected_axis);
 
 // Refactor this to pointer operations afterwards to flex 
 void setup_cube(int cube_slab[12][9]) {
@@ -138,7 +154,16 @@ void print_cube(int cube_slab[12][9]) {
                 printf(" ");
                 for (int j = 0; j < 9; j++){
                         if (cube_slab[0][j] != 0) {
-                                printf("%s%s%s%s", COLORS[cube_slab[i][j]-1], BLOCK, BLOCK, RESET);
+                                int color_value = cube_slab[i][j];
+                                int is_highlighted = (color_value & (1 << 7)) != 0;  // Check if the 8th bit is set
+                                if (is_highlighted) {
+                                        color_value &= ~(1 << 7);  // Clear the 8th bit to get the original color value
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK_HALF, BLOCK_HALF, RESET);
+                                }
+                                else {
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK, BLOCK, RESET);
+                                }
+
                                 if (j == 5) {
                                         printf("|");
                                 }
@@ -157,7 +182,15 @@ void print_cube(int cube_slab[12][9]) {
                         if (j == 0 || j == 3 || j == 6) {
                                 printf("|");
                         }
-                        printf("%s%s%s%s", COLORS[cube_slab[i][j]-1], BLOCK, BLOCK, RESET);
+                        int color_value = cube_slab[i][j];
+                        int is_highlighted = (color_value & (1 << 7)) != 0;  // Check if the 8th bit is set
+                        if (is_highlighted) {
+                                color_value &= ~(1 << 7);  // Clear the 8th bit to get the original color value
+                                printf("%s%s%s%s", COLORS[color_value-1], BLOCK_HALF, BLOCK_HALF, RESET);
+                        }
+                        else {
+                                printf("%s%s%s%s", COLORS[color_value-1], BLOCK, BLOCK, RESET);
+                        }
                         
                 }
                 printf("|\n");
@@ -167,7 +200,15 @@ void print_cube(int cube_slab[12][9]) {
                 printf(" ");
                 for (int j = 0; j < 9; j++){
                         if (cube_slab[0][j] != 0) {
-                                printf("%s%s%s%s", COLORS[cube_slab[i][j]-1], BLOCK, BLOCK, RESET);
+                                int color_value = cube_slab[i][j];
+                                int is_highlighted = (color_value & (1 << 7)) != 0;  // Check if the 8th bit is set
+                                if (is_highlighted) {
+                                        color_value &= ~(1 << 7);  // Clear the 8th bit to get the original color value
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK_HALF, BLOCK_HALF, RESET);
+                                }
+                                else {
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK, BLOCK, RESET);
+                                }
                                 if (j == 5) {
                                         printf("|");
                                 }
@@ -185,7 +226,15 @@ void print_cube(int cube_slab[12][9]) {
                 printf(" ");
                 for (int j = 0; j < 9; j++){
                         if (cube_slab[0][j] != 0) {
-                                printf("%s%s%s%s", COLORS[cube_slab[i][j]-1], BLOCK, BLOCK, RESET);
+                                int color_value = cube_slab[i][j];
+                                int is_highlighted = (color_value & (1 << 7)) != 0;  // Check if the 8th bit is set
+                                if (is_highlighted) {
+                                        color_value &= ~(1 << 7);  // Clear the 8th bit to get the original color value
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK_HALF, BLOCK_HALF, RESET);
+                                }
+                                else {
+                                        printf("%s%s%s%s", COLORS[color_value-1], BLOCK, BLOCK, RESET);
+                                }
                                 if (j == 5) {
                                         printf("|");
                                 }
@@ -199,10 +248,7 @@ void print_cube(int cube_slab[12][9]) {
                 printf(" \n");
         }
         printf("       +------+       \n");
-
-
 }
-
 
 void rotate_cube(int axis, char direction) {
         int dir = 1;
@@ -275,6 +321,51 @@ void shift_array_3(int *array[], int direction) {
         }
 }
 
+void toggle_highlight(int **selected_axis) {
+        for (int i = 0; i < 12; i++) {
+                if ((*(selected_axis[i]) & (1 << 7)) != 0) // Highlight off 
+                {
+                        *(selected_axis[i]) &= ~(1 << 7);  // Set the 8th bit to 0
+                }
+                else // Highlight on
+                {
+                        *(selected_axis[i]) |= (1 << 7);  // Set the 8th bit (MSB) to indicate highlighting
+                }
+        }
+}
+
+void highlight_axis_num(int axis) {
+        switch (axis) {
+                case 1:
+                        toggle_highlight(axis_1);
+                        break;
+                case 2:
+                        toggle_highlight(axis_2);
+                        break;
+                case 3:
+                        toggle_highlight(axis_3);
+                        break;
+                case 4:
+                        toggle_highlight(axis_4);
+                        break;
+                case 5:
+                        toggle_highlight(axis_5);
+                        break;
+                case 6:
+                        toggle_highlight(axis_6);
+                        break;
+                case 7:
+                        toggle_highlight(axis_7);
+                        break;
+                case 8:
+                        toggle_highlight(axis_8);
+                        break;
+                case 9:
+                        toggle_highlight(axis_9);
+                        break;
+        }
+        return;
+}
 
 
 // Basic functions ---------------------
